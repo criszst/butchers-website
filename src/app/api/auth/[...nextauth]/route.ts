489ba95bd5fa839be239246faa22d0073e32469b
Cristian <adrian.cristian.st@gmail.com>
2025-07-09 13:03:51 -0300

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,5 @@
+import { authOptions } from "@/app/utils/authOptions";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (user) token.id = user.id
-
-      if (account) token.accessToken = account.accessToken
-
-      return token
-    },
-    async session({ session, token }) {
-      session.user.id = token.id
-
-      session.accessToken = token.accessToken
-
-      return session
-    },
-    async redirect({ url, baseUrl }) {
-      return baseUrl
-    }
-  }
-}
 
 const handler = NextAuth(authOptions)
 
